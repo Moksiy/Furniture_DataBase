c2dea8850db03fbb670091f29e100c7ff490804a
Maks Aligin <maksaligin@gmail.com>
2020-03-03 21:04:20 +0300

    Login/Registration

---
+++ LogInRegistr/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureDataBase_WS
+{
+    static class Data
+    {
+        public static string Login { get; set; }
+        public static string Password { get; set; }
+        public static string Role { get; set; }
+
+    }
+}
