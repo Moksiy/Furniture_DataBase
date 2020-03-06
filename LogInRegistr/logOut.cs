13c954ebd777f4f68d6d5ce181cd3727638f2e9b
Moksiy <maksaligin@gmail.com>
2020-03-06 10:18:44 +0300

    Logout

---
+++ LogInRegistr/logOut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureDataBase_WS
+{
+    public static class logOut
+    {
+        public static void logoutUser()
+        {
+            Data.LastName = "";
+            Data.Login = "";
+            Data.Name = "";
+            Data.Password = "";
+            Data.Patronum = "";
+            Data.Role = "";
+        }
+    }
+}
